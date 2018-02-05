71fa6d3a5f3c842462656fb34ca8242616fe6236
Matt Boutell <boutell@gmail.com>
2018-02-05 16:32:32 -0500

    Start firestorm example

--- FavoriteThings/app/src/main/java/edu/rosehulman/favoritethings/MainActivity.java
+++ FavoriteThings/app/src/main/java/edu/rosehulman/favoritethings/MainActivity.java
@@ -5,17 +5,30 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private TextView mColorTextView;
     private TextView mNumberTextView;
     private long mNumber;
     private static final String TAG = "FAVES";
+    private static final String COLOR_KEY = "color";
+    private static final String NUMBER_KEY = "number";
+
+    private DocumentReference mDocRef;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDocRef =  FirebaseFirestore.getInstance().document("FavoriteThings/pNfAd1eN4anDtAg2CLqi");
 
         mColorTextView = (TextView) findViewById(R.id.color_text_view);
         mNumberTextView = (TextView) findViewById(R.id.number_text_view);
@@ -31,32 +44,43 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View view) {
+        Map<String, Object> dataToSave = new HashMap<>();
+
+
         switch (view.getId()) {
             case R.id.red_button:
                 mColorTextView.setText(R.string.red);
-
-                return;
+                dataToSave.put(COLOR_KEY, "red");
+                break;
             case R.id.white_button:
                 mColorTextView.setText(R.string.white);
-
-                return;
+                dataToSave.put(COLOR_KEY, "white");
+                break;
             case R.id.blue_button:
                 mColorTextView.setText(R.string.blue);
-
-                return;
+                dataToSave.put(COLOR_KEY, "blue");
+                break;
             case R.id.update_color_button:
                 Log.d(TAG, "Updating from Firebase");
-
-                return;
+                break;
             case R.id.increment_number_button:
                 mNumber++;
                 mNumberTextView.setText("" + mNumber);
-                return;
+                dataToSave.put(NUMBER_KEY, mNumber);
+                break;
             case R.id.decrement_number_button:
                 mNumber--;
                 mNumberTextView.setText("" + mNumber);
-                return;
-
+                dataToSave.put(NUMBER_KEY, mNumber);
+                break;
         }
+        mDocRef.update(dataToSave).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+
+            }
+        });
+
+
     }
 }
